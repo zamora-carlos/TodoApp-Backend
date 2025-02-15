fa1fbbc92501a50c07e19f1b3c1cf7648b7a539a
Carlos Zamora <uriel.zamora@encora.com>
2025-02-15 17:40:58 -0600

    feat: add validation rules to UpdateTodoRequest and CreateTodoRequest DTOs

--- src/main/java/com/example/todo/dto/UpdateTodoRequest.java
+++ src/main/java/com/example/todo/dto/UpdateTodoRequest.java
@@ -1,6 +1,9 @@
 package com.example.todo.dto;
 
 import com.example.todo.enums.Priority;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +15,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class UpdateTodoRequest implements Serializable {
+
+    @Size(min = 3, max = 120, message = "Text should be between 3 and 120 characters.")
     private String text;
+
     private Priority priority;
+
+    @FutureOrPresent(message = "Due date must be either today or in the future.")
     private LocalDateTime dueDate;
 }
